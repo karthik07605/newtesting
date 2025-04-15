2f0f5de0a250aa36f767365a4dd8b198454fcef9
karthik <karthik07605@gmail.com>
2025-04-15 18:11:39 +0530

    Your commit message describing the changes

--- lobby/settings.py
+++ lobby/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = 'django-insecure-*i^)-m667i7fh^9m2q(gy*yuct61i2#9@-^h&qpl@c42d)7+_+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True   
 
-ALLOWED_HOSTS = ['https://code-demo-82rc.onrender.com']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'code-demo-82rc.onrender.com',  # Add your domain name here
+]
 
 
 # Application definition
